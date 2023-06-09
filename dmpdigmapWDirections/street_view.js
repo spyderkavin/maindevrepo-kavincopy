a15c918ad37b9ac5971bb8bf99164036965aac48
Vasista R <vasista.pioneer@gmail.com>
2023-06-09 12:55:56 -0700

    Added Street View.js file. Added pannellum library code. Added library to index.html. Nothing visible as of now.
    v1.0

--- dmpdigmapWDirections/street_view.js
+++ dmpdigmapWDirections/street_view.js
@@ -1,29 +1,5 @@
-function initialize() {
-  const fenway = { lat: 42.345573, lng: -71.098326 };
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      center: fenway,
-      zoom: 14,
-    }
-  );
-  const panorama = new google.maps.StreetViewPanorama(
-    document.getElementById("pano") as HTMLElement,
-    {
-      position: fenway,
-      pov: {
-        heading: 34,
-        pitch: 10,
-      },
-    }
-  );
-
-  map.setStreetView(panorama);
-}
-
-declare global {
-  interface Window {
-    initialize: () => void;
-  }
-}
-window.initialize = initialize;
+pannellum.viewer('panorama-360-view', {
+  "type": "equirectangular",
+  "panorama": "https://i.imgur.com/sIOt8vR.jpeg",
+  "autoLoad": true
+})
