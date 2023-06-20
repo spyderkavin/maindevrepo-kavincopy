1b0365d997f8f234a8cb8d65721b4853a4850bfa
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-06-19 21:22:49 -0700

    Upgrade to Andriod 14

--- MHSDigitalMapProject5/app/src/main/java/com/example/mhsdigitalmapproject/MainActivity.java
+++ MHSDigitalMapProject5/app/src/main/java/com/example/mhsdigitalmapproject/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mywebView=(WebView) findViewById(R.id.webview);
         mywebView.setWebViewClient(new WebViewClient());
-        mywebView.loadUrl("https://dmpdigmapwdirections.cocoazi.repl.co");
+        mywebView.loadUrl("https://dmpdigmapphone.cocoazi.repl.co");
         WebSettings webSettings=mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         FirebaseMessaging.getInstance().subscribeToTopic("notifier")
