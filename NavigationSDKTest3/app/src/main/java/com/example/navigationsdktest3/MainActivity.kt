86228ad9b08dcc374bf8e423756d3b1c325d6fc2
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-06-30 13:07:04 -0700

    Removed a lot of the MNavigation SDK Initialization since it was disrupting the code

--- NavigationSDKTest3/app/src/main/java/com/example/navigationsdktest3/MainActivity.kt
+++ NavigationSDKTest3/app/src/main/java/com/example/navigationsdktest3/MainActivity.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.maps.MapView
-//import com.mapbox.maps.Style
+import com.mapbox.maps.Style
 
 var mapView: MapView? = null
 
@@ -12,16 +12,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
         mapView?.getMapboxMap()?.loadStyleUri(getString(R.string.mapbox_map_style))
-        setContentView(R.layout.activity_main)
     }
     @SuppressLint("Lifecycle")
     override fun onStart() {
         super.onStart()
         mapView?.onStart()
     }
-
 
     @SuppressLint("Lifecycle")
     override fun onStop() {
