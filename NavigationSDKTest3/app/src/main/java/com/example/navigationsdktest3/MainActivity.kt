6452c47383b1bd117d9effe50ff52b96f7d363de
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-06-30 00:24:36 -0700

    Commented import com.mapbox.maps.Style so it does not waste storgae pulling unused imports.

--- NavigationSDKTest3/app/src/main/java/com/example/navigationsdktest3/MainActivity.kt
+++ NavigationSDKTest3/app/src/main/java/com/example/navigationsdktest3/MainActivity.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.maps.MapView
-import com.mapbox.maps.Style
+//import com.mapbox.maps.Style
 
 var mapView: MapView? = null
 
