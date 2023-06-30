01978514f8d77aca2406b262d78d5245b56c43ac
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-06-29 17:29:41 -0700

    Bug Fix before major changes
    Updated my own Android studio to the hedgehog version, so I get a alpha version of the AGP. That way It has compatibility with Android 14(API Level 34). Changed logo(cuz why not?)

--- NavigationSDKTest3/app/src/main/java/com/example/navigationsdktest3/MainActivity.kt
+++ NavigationSDKTest3/app/src/main/java/com/example/navigationsdktest3/MainActivity.kt
@@ -1,37 +1,55 @@
 package com.example.navigationsdktest3
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+//import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.maps.MapView
-import com.mapbox.maps.Style
+//import com.mapbox.maps.Style
 
-var mapView: MapView? = null
+
+//var mapView: MapView? = null
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mapView: MapView
+    //private lateinit var map: MapboxMap
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
-        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+        mapView?.getMapboxMap()?.loadStyleUri(getString(R.string.mapbox_style))
+        
+
     }
+    @SuppressLint("Lifecycle")
     override fun onStart() {
         super.onStart()
         mapView?.onStart()
     }
 
+    @SuppressLint("Lifecycle")
     override fun onStop() {
         super.onStop()
         mapView?.onStop()
     }
 
+    @SuppressLint("Lifecycle")
     override fun onLowMemory() {
         super.onLowMemory()
         mapView?.onLowMemory()
     }
 
+    @SuppressLint("Lifecycle")
     override fun onDestroy() {
         super.onDestroy()
         mapView?.onDestroy()
     }
+
+
 }
+
