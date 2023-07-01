a7a14c7c08f368a2e5ec82cd4534df8f21d7a0e1
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-06-30 23:41:55 -0700

    Added Lifecycle

--- MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
+++ MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
@@ -15,4 +15,23 @@ class MainActivity : AppCompatActivity() {
         mapView = findViewById(R.id.mapView)
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
     }
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
 }
