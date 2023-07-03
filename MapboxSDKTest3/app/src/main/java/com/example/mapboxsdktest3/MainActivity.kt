5598a3935da11efbe08719eb5222c94799e05a8b
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-07-03 16:32:35 -0700

    Update MainActivity.kt

--- MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
+++ MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mapboxsdktest3
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.maps.MapView
@@ -18,26 +19,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+
+        //App Center
         AppCenter.start(
             application, "4c687564-51b0-4d7c-97bb-1c957b769ed9",
             Analytics::class.java, Crashes::class.java
         )
     }
+    @SuppressLint("Lifecycle")
     override fun onStart() {
         super.onStart()
         mapView?.onStart()
     }
 
+    @SuppressLint("Lifecycle")
     override fun onStop() {
         super.onStop()
         mapView?.onStop()
     }
 
+    @SuppressLint("Lifecycle")
     override fun onLowMemory() {
         super.onLowMemory()
         mapView?.onLowMemory()
     }
 
+    @SuppressLint("Lifecycle")
     override fun onDestroy() {
         super.onDestroy()
         mapView?.onDestroy()
