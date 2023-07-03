87caad8e07a8178c921369257af71e5f31f3a2fd
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-07-02 22:31:32 -0700

    Added Microsoft App Center SDK

--- MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
+++ MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 var mapView: MapView? = null
 
@@ -14,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+        AppCenter.start(
+            application, "4c687564-51b0-4d7c-97bb-1c957b769ed9",
+            Analytics::class.java, Crashes::class.java
+        )
     }
     override fun onStart() {
         super.onStart()
