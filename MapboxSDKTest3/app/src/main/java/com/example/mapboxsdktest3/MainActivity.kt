14495518826e4a69b4c6020e0cf14ec2f696b7f9
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-07-14 22:53:58 -0700

    Commented Style Link TEMPORARILY for convene

--- MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
+++ MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
@@ -22,7 +22,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
-        mapView?.getMapboxMap()?.loadStyleUri(Style.SATELLITE)
+        //Style link commeneted here for conveinence purposes
+        //val STYLE_URL = "mapbox://styles/spyderkavin/cli1anwx900v901poaa5e9cxg"
+        mapView?.getMapboxMap()?.loadStyleUri(getString(R.string.STYLE_URL))
 
 /*
         //App Center
