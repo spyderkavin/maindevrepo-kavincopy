334d8a2fbff76466575631f2435a2f9a1f579d35
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-07-14 21:15:20 -0700

    Begin to initialize SDK

--- NavigationSDKTest5/app/src/main/java/com/example/navigationsdktest5/MainActivity.kt
+++ NavigationSDKTest5/app/src/main/java/com/example/navigationsdktest5/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.navigationsdktest5
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.navigation.base.options.NavigationOptions
+import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (!MapboxNavigationApp.isSetup()) {
+            MapboxNavigationApp.setup {
+                NavigationOptions.Builder(context)
+                    .accessToken("YOUR_ACCESS_TOKEN")
+                    // additional options
+                    .build()
+            }
+        }
     }
 }
