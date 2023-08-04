ff80f9f6da1e86e8f6a2f1157642d75dbdaccc0a
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2023-08-03 22:04:26 -0700

    REMOVING FIREBASE

--- MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
+++ MapboxSDKTest3/app/src/main/java/com/example/mapboxsdktest3/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.messaging.Constants.MessageNotificationKeys.TAG
 import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
@@ -32,40 +31,6 @@ class MainActivity : AppCompatActivity() {
         //val STYLE_URL = "mapbox://styles/spyderkavin/cli1anwx900v901poaa5e9cxg"
         mapView?.getMapboxMap()?.loadStyleUri(getString(R.string.STYLE_URL))
     }
-
-
-
-    // [START ask_post_notifications]
-    // Declare the launcher at the top of your Activity/Fragment:
-        private val requestPermissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestPermission(),
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                // FCM SDK (and your app) can post notifications.
-            } else {
-                // TODO: Inform user that that your app will not show notifications.
-            }
-        }
-
-        private fun askNotificationPermission() {
-            // This is only necessary for API level >= 33 (TIRAMISU)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
-                    PackageManager.PERMISSION_GRANTED
-                ) {
-                    // FCM SDK (and your app) can post notifications.
-                } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                    // TODO: display an educational UI explaining to the user the features that will be enabled
-                    //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                    //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                    //       If the user selects "No thanks," allow the user to continue without notifications.
-                } else {
-                    // Directly ask for the permission
-                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-                }
-            }
-        }
-    // [END ask_post_notifications]
 
     @SuppressLint("Lifecycle")
     override fun onStart() {
